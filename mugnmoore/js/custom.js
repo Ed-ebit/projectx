952f8c94e8dd616bdac48d50ab11f65e4b97ce75
master <master@desaster.com>
2021-09-06 14:56:12 +0200

    neu

---
+++ mugnmoore/js/custom.js
@@ -0,0 +1,33 @@
+/* Start Klassenänderung */
+$(window).on('scroll', function(){
+    if( $(window).scrollTop() > 50 ){
+        //Navigation
+        $('nav.navbar').addClass('bg-white');
+        // Back to top button
+        $('#backtotopbutton').addClass('d-inline');
+    } 
+    else {
+        //Navigation back
+        $('nav.navbar').removeClass('bg-white');
+        // Back to top button entfernen
+        $('#backtotopbutton').removeClass('d-inline');
+    }
+});
+/* Ende Klassenänderung */
+
+/* Start Smooth Scroll */
+// Sicherstellen, dass alle DOM Elemente geladen sind
+$(document).ready(function(){
+    $('a.smooth').on('click', function(event) {
+        if(this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+});
+/* Ende Smooth Scroll */
