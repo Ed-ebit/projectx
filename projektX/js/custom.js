0914cd7475816d1733246033a445e53bfcae11dd
master <master@desaster.com>
2021-09-08 15:02:31 +0200

    8.9.

--- projektX/js/custom.js
+++ projektX/js/custom.js
@@ -1,10 +1,17 @@
 
 /* === Sidebar Nav. === */
 
-var menu_btn = document.querySelector("#menu-btn");
-var sidebar = document.querySelector("#sidebar");
-var container = document.querySelector(".my-container");
-menu_btn.addEventListener("click", () => {
+$(document).ready(function(){
+  var menu_btn = $("#menu-btn");
+  var sidebar = $("#sidebar");
+  var container = $(".my-container");
+  menu_btn.click(function(){
+    sidebar.toggleClass("active-nav");
+    container.toggleClass("active-cont");
+  });
+});
+
+/* menu_btn.addEventListener("click", () => {
   sidebar.classList.toggle("active-nav");
   container.classList.toggle("active-cont");
-});
+}); */
